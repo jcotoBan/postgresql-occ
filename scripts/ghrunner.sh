d25cc15f11cdd5e52130442f14e99b85bf984edd
Juan Coto <jcotsan@gmail.com>
2023-09-29 12:35:20 -0600

    testing

--- scripts/ghrunner.sh
+++ scripts/ghrunner.sh
@@ -22,7 +22,7 @@ create_runner () {
         "swap_size": 512,
         "image": "linode/ubuntu22.04",
         "root_pass": "'"${root_password}"'",
-        "stackscript_id": 1112851,
+        "stackscript_id": 1244472,
         "stackscript_data": {
             "repository": "'"${repo}"'",
             "gh_password": "'"${registration_token}"'",
