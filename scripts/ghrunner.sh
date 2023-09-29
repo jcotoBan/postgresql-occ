16629e16d3c5c9a53932e478b331d13222fb1d07
Juan Coto <jcotsan@gmail.com>
2023-09-29 12:39:06 -0600

    testing

--- scripts/ghrunner.sh
+++ scripts/ghrunner.sh
@@ -25,8 +25,7 @@ create_runner () {
         "stackscript_id": 1244472,
         "stackscript_data": {
             "repository": "'"${repo}"'",
-            "gh_password": "'"${registration_token}"'",
-            "owner": "'"${owner}"'"
+            "gh_password": "'"${registration_token}"'"
         },
         "authorized_keys": [
             "ssh-rsa AAAA_valid_public_ssh_key_123456785== user@their-computer"
