7615bb70a557ef73b17ff26003b682f9c4fb96c4
Juan Coto <jcotsan@gmail.com>
2023-10-06 10:09:42 -0600

    testing

--- scripts/ghrunner.sh
+++ scripts/ghrunner.sh
@@ -7,13 +7,10 @@ readonly repo="${2}"
 readonly date=$(date '+%Y-%m-%d_%H%M%S')
 
 runner_token () {
-    echo "${linode_token}"
-    echo "${gh_token}"
     registration_token=$(curl -sX POST -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${gh_token}"\
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/${owner}/${repo}/actions/runners/registration-token | jq -r .token)
-    echo "${registration_token}"
 }
 
 create_runner () {
