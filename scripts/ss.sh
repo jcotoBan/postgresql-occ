2d267407054033575dd501d0406ffda9b211580b
Juan Coto <jcotsan@gmail.com>
2023-11-28 07:49:28 -0600

    testing changing the domain of the cluster

--- scripts/ss.sh
+++ scripts/ss.sh
@@ -16,7 +16,7 @@ trap "cleanup $? $LINENO" EXIT
 
 # ...
 
-export cluster_name="test_postgres_cluster"
+export cluster_name="jcoto.com"
 export sudo_username="testuser"
 export add_ssh_keys="yes"
 export cluster_size="3"
