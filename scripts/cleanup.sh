33cdf8b5ce13787c7dae196326cfd6baae5b2ea8
Juan Coto <jcotsan@gmail.com>
2023-11-29 10:24:30 -0600

    testing

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -10,12 +10,15 @@ readonly date=$(date '+%Y-%m-%d_%H%M%S')
 
 
 remove_runner () {
+    
+    cd /actions-runner/
+
     registration_token=$(curl -sX POST -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer ${gh_token}"\
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/${owner}/${repo}/actions/runners/registration-token | jq -r .token)
 
-    ./config.sh remove --token $registration_token
+    RUNNER_ALLOW_RUNASROOT="1" ./config.sh remove --token $registration_token
 }
 
 remove_runner
